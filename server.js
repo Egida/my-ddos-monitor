c3386cb44cd4bfe7477a9b13f5d6896de3c3107c
w3ngjin <w320656@163.com>
2024-04-27 10:37:35 +0800

    不用python

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 3001; // 使用环境变量或默认值设置�
 
 const { getBlockedIPs } = require('./ipBlockList');
 
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
-exec('python3 ack_monitor.py', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-});
+// exec('python3 ack_monitor.py', (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+// });
 
 
 app.use((req, res, next) => {
